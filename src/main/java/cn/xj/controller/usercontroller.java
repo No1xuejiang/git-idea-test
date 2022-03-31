628a9e7259ebbf6df9d71db9b0d42272a92cbb15
xuejiang <xj@qq.com>
2022-03-31 21:26:21 +0800

    第一次dev分支提交

--- src/main/java/cn/xj/controller/usercontroller.java
+++ src/main/java/cn/xj/controller/usercontroller.java
@@ -3,5 +3,6 @@ package cn.xj.controller;
 public class usercontroller {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
+        System.out.println("hello git dev!!!");
     }
 }
